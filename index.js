00d2d404d1f546d42ed5d1c87cfa83db034d0c22
Steven Almeroth <sroth77@gmail.com>
2021-02-04 16:38:52 -0500

    external data

--- index.js
+++ index.js
@@ -1,72 +1,22 @@
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
-import * as am4plugins_forceDirected from "@amcharts/amcharts4/plugins/forceDirected";
+import * as am4core from "@amcharts/amcharts4/core"
+import * as am4charts from "@amcharts/amcharts4/charts"
+import * as am4plugins_forceDirected from "@amcharts/amcharts4/plugins/forceDirected"
 
-// Create chart
-let chart = am4core.create("chartdiv", am4plugins_forceDirected.ForceDirectedTree);
-
-// Create series
+let chart = am4core.create("chartdiv", am4plugins_forceDirected.ForceDirectedTree)
 let series = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries())
 
-// Set data
-series.data = [{
-  "name": "First",
-  "children": [{
-    "name": "A1", "value": 100
-  }, {
-    "name": "A2", "value": 60
-  }, {
-    "name": "A3", "value": 30
-  }]
-}, {
-  "name": "Second",
-  "children": [{
-    "name": "B1", "value": 135
-  }, {
-    "name": "B2", "value": 98
-  }, {
-    "name": "B3", "value": 56
-  }]
-}, {
-  "name": "Third",
-  "children": [{
-    "name": "C1", "value": 335
-  }, {
-    "name": "C2", "value": 148
-  }, {
-    "name": "C3", "value": 126
-  }, {
-    "name": "C4", "value": 26
-  }]
-}, {
-  "name": "Fourth",
-  "children": [{
-    "name": "D1", "value": 415
-  }, {
-    "name": "D2", "value": 148
-  }, {
-    "name": "D3", "value": 89
-  }, {
-    "name": "D4", "value": 64
-  }, {
-    "name": "D5", "value": 16
-  }]
-}, {
-  "name": "Fifth",
-  "children": [{
-    "name": "E1", "value": 687
-  }, {
-    "name": "E2", "value": 148
-  }]
-}];
+// Set up data source
+series.dataSource.url = "data.json"
 
 // Set up data fields
-series.dataFields.value = "value";
-series.dataFields.name = "name";
-series.dataFields.children = "children";
+series.dataFields.id = "id"
+series.dataFields.name = "name"
+series.dataFields.value = "value"
+series.dataFields.linkWith = "link"
+series.dataFields.children = "children"
 
 // Add labels
-series.nodes.template.label.text = "{name}";
-series.fontSize = 10;
-series.minRadius = 15;
-series.maxRadius = 40;
+series.nodes.template.label.text = "({id})\n{name}"
+series.fontSize = 10
+series.minRadius = 15
+series.maxRadius = 40
