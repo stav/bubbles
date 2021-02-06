948e2aa5daf800c1b098c0f6c9f1be5d1958e690
Steven Almeroth <sroth77@gmail.com>
2021-02-06 13:52:04 -0500

    cleanup

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-import * as am4core from "@amcharts/amcharts4/core"
-import * as am4charts from "@amcharts/amcharts4/charts"
-import * as am4plugins_forceDirected from "@amcharts/amcharts4/plugins/forceDirected"
+import * as am4core from '@amcharts/amcharts4/core'
+import * as am4plugins_forceDirected from '@amcharts/amcharts4/plugins/forceDirected'
 
 // Chart
-const chart = am4core.create("chartdiv", am4plugins_forceDirected.ForceDirectedTree)
+const chart = am4core.create('chartdiv', am4plugins_forceDirected.ForceDirectedTree)
 
 // Data series
 const series = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries())
 // Data source
-series.dataSource.url = "data.json"
+series.dataSource.url = 'data.json'
 // Data fields
-series.dataFields.id = "id"
-series.dataFields.name = "name"
-series.dataFields.value = "value"
-series.dataFields.linkWith = "link"
-series.dataFields.children = "children"
-series.dataFields.collapsed = "off"
+series.dataFields.id = 'id'
+series.dataFields.name = 'name'
+series.dataFields.value = 'value'
+series.dataFields.linkWith = 'link'
+series.dataFields.children = 'children'
+series.dataFields.collapsed = 'off'
 // Appearance
 series.fontSize = 10
 series.maxLevels = 3
 series.minRadius = 30
 series.maxRadius = 100
 series.nodes.template.expandAll = false
-series.nodes.template.label.text = "({id})\n{name}"
+series.nodes.template.label.text = '{id}\n{name}'
